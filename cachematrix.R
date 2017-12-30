16e5f38caf1c5a9af73b8703ccc44e2307ac2ebe
venkatk <venkateswaran_kasirajan@trimble.com>
2017-12-30 16:45:10 -0700

    Solved

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function creates a cache of a matrix and its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
 
+    myInverse <- NULL
+
+    get <- function() x
+
+    set <- function(y) {
+        myInverse = NULL;
+        x <<- y
+    }
+
+    setinverse <- function(inverse) {
+        myInverse <<- inverse
+    }
+
+    getinverse <- function() {
+        myInverse
+    }
+
+    list(
+        get = get,
+       set = set,
+       setinverse = setinverse,
+       getinverse = getinverse
+    )
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of a matrix
+## If the inverse is already calculated, it just returns that value
+## Otherwise, the inverse is calculated and cached
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    inverse <- x$getinverse()
+    if (is.null(inverse)) {
+        message("Calculating Inverse")      ## Inverse needs to be calculated
+        data <- x$get()
+        inverse <- solve(data, ...)         ## Find the inverse
+        x$setinverse(inverse)
+        return (inverse)
+    }
+    message("Fetching cached Inverse")      ## If we get here, the Inverse was already calculated and avaiable.
+    return(inverse)
 }
